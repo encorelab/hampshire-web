85a3fd81c9ebd18a7f6b9fca51d231fb4173ca38
switchflip <thom.berry@gmail.com>
2014-07-09 11:42:13 -0400

    Working on active.js

--- source/javascripts/active.js
+++ source/javascripts/active.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     var url = window.location;
     // Will only work if string in href matches with location
-    $('ul.nav a[href="'+ url +'"]').parent().addClass('active');
+    // $('ul.nav a[href="'+ url +'"]').parent().addClass('active');
 
     // Will also work for relative and absolute hrefs
     $('ul.nav a').filter(function() {
