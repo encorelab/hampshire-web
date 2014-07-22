3f9ddcb4a51c6fcacec962bf561885948ed5d6a0
megseoh <ohara.meagan@gmail.com>
2014-07-22 15:37:00 -0400

    undid second trivial change

--- source/javascripts/hampshire.js
+++ source/javascripts/hampshire.js
@@ -5,7 +5,7 @@
 
   app.init = function() {
     // this function is execute on start and can call other function
-    // usually a lot of the hooks are places here again
+    // usually a lot of the hooks are places here
     console.log('this is where it all starts');
 
     setupClickListeners();
